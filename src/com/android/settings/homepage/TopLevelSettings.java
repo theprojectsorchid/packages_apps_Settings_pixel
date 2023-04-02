99b193a849cbae7833b6aa95eb3f46d317ec2789
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-04-02 20:12:57 +0000

    OrchidOs Settings dashboard : Top level settings adjustments
    OrchidOs Settings dashboard : Top level settings adjustments
    improve the distance between the cards add summary cards
    adjust the cards and remove summarys for most items leave storage location and battery for now hide anatolia settings as it is now remodeled and used for other uses
    toplevel dash board hide my device info fragment and fix summarys for a few things
    add summarys for storage battery and location if google app is there add also
    Update TopLevelSettings.java
    fix up google and wellbieng cards and move system navigation forward and upward

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -247,7 +247,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.xd_pref_card_bot);
             }
             if (key.equals("top_level_accounts")){
-                preference.setLayoutResource(R.layout.xd_pref_card_bot);
+                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
             }
             if (key.equals("top_level_octavi_lab")){
                 preference.setLayoutResource(R.layout.xd_pref_card_top);
@@ -256,7 +256,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.xd_pref_card_bot);
             }
             if (key.equals("top_level_battery")){
-                preference.setLayoutResource(R.layout.xd_pref_card_top);
+                preference.setLayoutResource(R.layout.xd_pref_card_topsummary);
             }
             if (key.equals("top_level_display")){
                 preference.setLayoutResource(R.layout.xd_pref_card_mid);
@@ -268,13 +268,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.xd_pref_card_top);
             }
             if (key.equals("top_level_storage")){
-                preference.setLayoutResource(R.layout.xd_pref_card_bot);
+                preference.setLayoutResource(R.layout.xd_pref_card_botsummary);
             }
             if (key.equals("top_level_notifications")){
                 preference.setLayoutResource(R.layout.xd_pref_card_bot);
             }
             if (key.equals("top_level_location")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid);
+                preference.setLayoutResource(R.layout.xd_pref_card_botsummary);
             }
             if (key.equals("top_level_accessibility")){
                 preference.setLayoutResource(R.layout.xd_pref_card_mid2);
@@ -301,22 +301,22 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.xd_pref_card_top);
             }
             if (key.equals("dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink")){
-                preference.setLayoutResource(R.layout.xd_pref_card_bot);
+                preference.setLayoutResource(R.layout.xd_pref_card_botsummary);
             }
 	        if (key.equals("top_level_google")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
+                preference.setLayoutResource(R.layout.xd_pref_card_botsummary);
             }
 	        if (key.equals("top_level_google")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
+                preference.setLayoutResource(R.layout.xd_pref_card_botsummary);
             }
 	        if (key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.home.TopLevelSettingsActivity")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
+                preference.setLayoutResource(R.layout.xd_pref_card_top);
             }
 	        if (key.equals("top_level_wellbeing")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
+                preference.setLayoutResource(R.layout.xd_pref_card_top);
             }
             if (key.equals("top_level_userinfo")){
-                preference.setLayoutResource(R.layout.xd_pref_card_mid2);
+                preference.setLayoutResource(R.layout.xd_pref_card_mid2summary);
             }   
 	}
     }
