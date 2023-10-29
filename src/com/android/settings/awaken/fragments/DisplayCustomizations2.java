theprojectsorchid/android_packages_apps_Settings

OrchidOs Import Settings: Add option to launch display customizations fragments
Change-Id: I301ac9416994449b941b5a9e0e9aa79a76f58281
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
import one for tap and one for phone buttons for now

OrchidOs Import Settings: Add option to launch display customizations

src/com/android/settings/awaken/fragments/DisplayCustomizations2.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Wave-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.awaken.fragments;
+
+import android.os.Bundle;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+public class DisplayCustomizations extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Display Customizations2";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.display_customizations2);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return -1;
+    }
+}

theprojectsorchid/packages_apps_Settings_pixel

OrchidOs Import Settings: Add option to launch display customizations we will use these for future static pages within settings
OrchidOs Ui Gestures : we will test one preference on Gesture settings
this is a port of awakens fragments

src/com/android/settings/awaken/fragments/DisplayCustomizations2.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Wave-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.awaken.fragments;
+
+import android.os.Bundle;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+public class DisplayCustomizations2 extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Display Customizations2";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.display_customizations2);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return -1;
+    }
+}