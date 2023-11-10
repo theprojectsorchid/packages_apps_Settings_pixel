theprojectsorchid/android_packages_apps_Settings

OrchidOs Import Settings : Anatolia fragments into Settings
we will import these extra fragments to be able to launce our static pages an other feature this is a backwards port from A13 that can work instead of building another external application we will not be using for what it was intended this is a simple workaround to keep settings still secure without having to read or implement more code from outside
Signed-off-by: Escobar1945 <candaroglu197878@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -168,6 +168,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.epic.Anatolia;
+
 public class SettingsGateway {
 
     /**
@@ -323,7 +325,9 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName()
+            MediaManagementAppsDetails.class.getName(),
+            // Anatolia Additions
+            Anatolia.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -364,5 +368,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // Anatolia Additions
+            Settings.AnatoliaSettingsActivity.class.getName(),
     };
 }

theprojectsorchid/packages_apps_Settings_pixel

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -183,6 +183,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
+import com.epic.Anatolia;
 
 public class SettingsGateway {
 
@@ -351,9 +352,9 @@ public class SettingsGateway {
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
             TurnScreenOnDetails.class.getName(),
-            LiveDisplaySettings.class.getName(),
-            WifiTetherClientManager.class.getName(),
-            LabSettings.class.getName()            
+            LabSettings.class.getName(),            
+            // Anatolia Additions
+            Anatolia.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -396,5 +397,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // Anatolia Additions
+            Settings.AnatoliaSettingsActivity.class.getName(),
     };
 }