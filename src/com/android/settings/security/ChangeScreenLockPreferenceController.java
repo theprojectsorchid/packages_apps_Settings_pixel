63be173e6d21454985df874bfadabbc7ff56a290
theprojects <talabijay@gmail.com>
2023-11-17 23:30:06 +0000

    Revert "Security Fragment: Adapt to DotUI"
    This reverts commit 009093d60c5dce55261af3012bef2848d7521907.

--- src/com/android/settings/security/ChangeScreenLockPreferenceController.java
+++ src/com/android/settings/security/ChangeScreenLockPreferenceController.java
@@ -79,7 +79,6 @@ public class ChangeScreenLockPreferenceController extends AbstractPreferenceCont
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mPreference = screen.findPreference(getPreferenceKey());
-	mPreference.setLayoutResource(R.layout.dot_preference_top_card);
     }
 
     @Override
