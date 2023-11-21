1cbb6832f51d73a587264f069e4a9646343c2cf0
theprojectsorchid <126561359+theprojectsorchid@users.noreply.github.com>
2023-11-21 12:53:32 +0000

    import more fragments

---
+++ src/com/palladium/atomichub/categories/frag_misc.java
@@ -0,0 +1,87 @@
+package com.palladium.atomichub.categories;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.content.Context;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+import android.os.ServiceManager;
+import com.palladium.atomichub.*;
+import android.app.ActionBar;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+import android.provider.SearchIndexableResource;
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+
+public class frag_misc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private IOverlayManager mOverlayService;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ps_misc);
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+        //Feature Additon!
+
+    }
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ORCHIDOS_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+
+    /**
+     * For Seaech
+     */
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.ps_misc;
+                    result.add(sir);
+                    return result;
+                }
+
+           @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
+
+}
