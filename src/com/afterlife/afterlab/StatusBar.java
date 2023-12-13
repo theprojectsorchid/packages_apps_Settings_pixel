f1bdcb7ca8f56d76eda4584913cde6128726fdca
theprojects <talabijay@gmail.com>
2023-12-13 20:15:08 +0000

    Update StatusBar.java

--- src/com/afterlife/afterlab/StatusBar.java
+++ src/com/afterlife/afterlab/StatusBar.java
@@ -16,7 +16,6 @@
 package com.afterlife.afterlab;
 
 import android.content.ContentResolver;
-import android.content.Intent;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
