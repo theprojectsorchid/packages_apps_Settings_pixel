9c83d82f66ada7a45fdd74be82d3dd45f9990634
theprojects <talabijay@gmail.com>
2023-12-23 14:21:03 +0000

    Revert "OrchidOs Avatar :  homepage"
    This reverts commit b959a45a8f54e9939afa93e6d75ab4b4fb276da5.

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,20 +1,16 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.content.Context;
-
-import com.android.settings.R;
-import com.android.settings.overlay.FeatureFactory;
 
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return FeatureFactory.getAppContext().getString(R.string.account_type);
+        return null;
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return AccountManager.get(context).getAccountsByType(getAccountType());
+        return new Account[0];
     }
 }
