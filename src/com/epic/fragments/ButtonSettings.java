a65c84548c73f0e0e0dee2d00a924bdd47c6db68
theprojectsorchid <126561359+theprojectsorchid@users.noreply.github.com>
2024-01-13 02:58:57 +0000

    create header for settings

--- src/com/epic/fragments/ButtonSettings.java
+++ src/com/epic/fragments/ButtonSettings.java
@@ -33,14 +33,56 @@ import androidx.preference.SwitchPreference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.EntityHeaderController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.settingslib.widget.LayoutPreference;
+import java.util.ArrayList;
+import java.util.List;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.UserInfo;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.UserManager;
+import android.util.Log;
+import android.view.View;
+import com.android.settings.Utils;
+import com.android.settings.deviceinfo.DeviceNamePreferenceController;
 
 public class ButtonSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+            private static final String LOG_TAG = "MyDeviceInfoFragment";
+            private static final String KEY_MY_DEVICE_INFO_HEADER = "my_device_info_header";
+        }
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            use(DeviceNamePreferenceController.class).setHost(this /* parent */);
+        }
+        
+        @Override
+        public void showDeviceNameWarningDialog(String deviceName) {
+            DeviceNameWarningDialog.show(this);
+        }
+    
+        public void onSetDeviceNameConfirm(boolean confirm) {
+            final DeviceNamePreferenceController controller = use(DeviceNamePreferenceController.class);
+            controller.updateDeviceName(confirm);
+        }
+    
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
